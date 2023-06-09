87e0950f7c1e5c6603e4e13e0c6055ce0f2eed7f
Yuancheng Zhang <old.far.hot@gmail.com>
2023-06-09 17:42:05 +0800

    [vite] update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import * as path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/hacker-gpt-website/",
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
