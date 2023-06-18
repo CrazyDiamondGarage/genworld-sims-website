e42ced7a5c2ef09db0151a8928adcf1ebf843e67
Yuancheng Zhang <old.far.hot@gmail.com>
2023-06-18 13:21:20 +0800

    [style] update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import React, { useState } from "react";
 import "./App.css";
 
 const App = () => {
+  const [playing, setPlaying] = useState(false);
+
   //! play game
   const playGame = () => {
     //* Play Game!!
@@ -8,22 +11,31 @@ const App = () => {
     const elem = document.getElementById("godot-game");
     if (elem) {
       elem.contentWindow.startGame();
+      setPlaying(true);
     } else {
       console.debug("Cannot find game :-(");
     }
   };
   return (
     <>
+      <div id="gw-top">
+        <div id="gw-logo">
+          <img src="/img/genworld.png" className="logo" alt="GenWorld" />
+        </div>
+        <button id="gw-btn-start-game" onClick={playGame} disabled={playing}>
+          RUN
+        </button>
+      </div>
+
+      <br />
+      <br />
+      <iframe id="godot-game" className={`game-iframe`} title="GenWorld" src="/game/game.html" />
+
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/img/godot.png" className="logo" alt="Vite logo" />
         </a>
       </div>
-      <h1>GenWorld</h1>
-      <button onClick={playGame}>Start Game</button>
-      <br />
-      <br />
-      <iframe id="godot-game" className={`game-iframe`} title="GenWorld" src="/game/game.html" />
     </>
   );
 };
