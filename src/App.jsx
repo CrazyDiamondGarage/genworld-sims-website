381a076e5a21bd504ea31572ccfa8b16581afccf
Yuancheng Zhang <old.far.hot@gmail.com>
2023-06-27 15:55:20 +0800

    [ft] show game screen shot in main page

--- src/App.jsx
+++ src/App.jsx
@@ -72,9 +72,14 @@ const App = () => {
       <br />
       <br />
 
-      <iframe id="godot-game" className="game-window" title="GenWorld" src="/game/game.html" />
+      <iframe
+        id="godot-game"
+        className={`${playing ? "" : "gw-hide"} game-window`}
+        title="GenWorld"
+        src="/game/game.html"
+      />
 
-      {/* <img className="game-window" src="/img/screenshot_01.png"></img> */}
+      <img className={`${playing ? "gw-hide" : ""} game-window`} src="/img/screenshot_01.png"></img>
 
       <div id="gw-middle" className={playing ? "" : "gw-hide"}>
         <button id="gw-btn-coke" onClick={codePlus}>
